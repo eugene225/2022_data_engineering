216a8bad46a664fd32f6b17a38585038b87b2e49
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-09-11 00:22:21 +0900

    App7 update

--- src/main/java/kr/ac/sejong/kmooc/data_engineering/App7.java
+++ src/main/java/kr/ac/sejong/kmooc/data_engineering/App7.java
@@ -6,96 +6,53 @@ import java.io.IOException;
 
 public class App7 {
 	
-	public static void app2() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\박유진\\Data\\email.txt"));
-		
-		int lineCount = 0;  //줄 수를 담는 line 변수
-		while(true) {
-			String line = br.readLine();
-			
-			if(line==null) break;
-			lineCount++;
-			if(line.startsWith("#")) continue;
-			
-			String[] ar = line.split("\t");
-			int from = Integer.parseInt(ar[0]);
-			int to = Integer.parseInt(ar[1]);
-			
-			//System.out.println(new Email(from, to));	
+	public static void app3(Email[] data) {
+		int size=0;
+		for(Email email:data) {
+			size++;
 		}
-		System.out.println("줄 수 : "+lineCount);
-		br.close();
+		System.out.println("줄 수 : "+size);
 	}
 	
-	public static void app3() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\박유진\\Data\\email.txt"));
-		
-		int eventCount = 0;  //줄 수를 담는 line 변수
-		while(true) {
-			String line = br.readLine();
-			
-			if(line==null) break;
-			if(line.startsWith("#")) continue;
-			eventCount++;
-			
-			String[] ar = line.split("\t");
-			int from = Integer.parseInt(ar[0]);
-			int to = Integer.parseInt(ar[1]);
-			
-			//System.out.println(new Email(from, to));	
-		}
-		System.out.println("줄 수 : "+eventCount);
-		br.close();
-	}
-	
-	public static void app4() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\박유진\\Data\\email.txt"));
-		
-		int min = Integer.MAX_VALUE;  //가장 최소의 사람 ID를 담는 min변수
-		while(true) {
-			String line = br.readLine();
-			
-			if(line==null) break;
-			if(line.startsWith("#")) continue;
-			
-			String[] ar = line.split("\t");
-			int from = Integer.parseInt(ar[0]);
-			if(from < min) min = from;
-			int to = Integer.parseInt(ar[1]);
-			if(to < min) min = to;
-			
-			//System.out.println(new Email(from, to));	
+	public static void app4(Email[] data) {
+		int min = Integer.MAX_VALUE;
+		for(Email email: data) {
+			if(email.from < min) min = email.from;
+			if(email.to < min) min = email.to;
 		}
 		System.out.println("최소 사람 ID : "+min);
-		br.close();
 	}
 	
-	public static void app5() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\박유진\\Data\\email.txt"));
-		
-		int max = Integer.MIN_VALUE;  //가장 최소의 사람 ID를 담는 max변수
-		while(true) {
-			String line = br.readLine();
-			
-			if(line==null) break;
-			if(line.startsWith("#")) continue;
-			
-			String[] ar = line.split("\t");
-			int from = Integer.parseInt(ar[0]);
-			if(from > max) max = from;
-			int to = Integer.parseInt(ar[1]);
-			if(to > max) max = to;
-			
-			//System.out.println(new Email(from, to));	
+	public static void app5(Email[] data) {
+		int max = Integer.MIN_VALUE;
+		for(Email email: data) {
+			if(email.from > max) max = email.from;
+			if(email.to > max) max = email.to;
 		}
 		System.out.println("최대 사람 ID : "+max);
-		br.close();
 	}
 	
 	public static void main(String args[]) throws IOException {
-		app2();
-		app3();
-		app4();
-		app5();
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\박유진\\Data\\email.txt"));
+		Email[] data = new Email[420045];
+		int index = 0;  //가장 최소의 사람 ID를 담는 max변수
+		while(true) {
+			String line = br.readLine();
+			
+			if(line==null) break;
+			if(line.startsWith("#")) continue;
+			
+			String[] ar = line.split("\t");
+			int from = Integer.parseInt(ar[0]);
+			int to = Integer.parseInt(ar[1]);
+			
+			data[index++] = new Email(from, to);
+			//System.out.println(new Email(from, to));	
+		}
+		br.close();
+		
+		app3(data);
+		app4(data);
+		app5(data);
 	}
 }
