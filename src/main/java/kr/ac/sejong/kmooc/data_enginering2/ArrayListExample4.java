e9239af59129e036734b8c7d6d75e890e013edad
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-09-18 00:45:18 +0900

    ArrayListExample4

---
+++ src/main/java/kr/ac/sejong/kmooc/data_enginering2/ArrayListExample4.java
@@ -0,0 +1,20 @@
+package kr.ac.sejong.kmooc.data_enginering2;
+
+import java.util.ArrayList;
+
+public class ArrayListExample4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		l.add(0, 3);
+		System.out.println(l);
+		l.add(1,5);
+		System.out.println(l);
+		l.add(2,7);
+		System.out.println(l);
+		l.add(1,9);
+		System.out.println(l);
+	}
+
+}
