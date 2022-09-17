d4ce47fdb254e9fe610b32fad5f815cf4be0bd6a
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-09-18 00:23:16 +0900

    arrayListExample2

---
+++ src/main/java/kr/ac/sejong/kmooc/data_enginering2/ArrayListExample1.java
@@ -0,0 +1,17 @@
+package kr.ac.sejong.kmooc.data_enginering2;
+
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		System.out.println(intList.isEmpty());
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		System.out.println(stringList.isEmpty());
+
+	}
+
+}
---
+++ src/main/java/kr/ac/sejong/kmooc/data_enginering2/ArrayListExample2.java
@@ -0,0 +1,19 @@
+package kr.ac.sejong.kmooc.data_enginering2;
+
+import java.util.ArrayList;
+
+public class ArrayListExample2 {
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		System.out.println(intList.isEmpty());
+		System.out.println(intList.size());
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		System.out.println(stringList.isEmpty());
+		System.out.println(intList.size());
+		
+	}
+
+}
