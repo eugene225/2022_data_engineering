c31758be56e3058b22d4cc7a9da37c084b01bead
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-09-18 17:33:57 +0900

    ArrayListExample15

--- src/main/java/kr/ac/sejong/kmooc/data_enginering2/ArrayListExample15.java
+++ src/main/java/kr/ac/sejong/kmooc/data_enginering2/ArrayListExample15.java
@@ -3,6 +3,7 @@ package kr.ac.sejong.kmooc.data_enginering2;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ListIterator;
 
 import kr.ac.sejong.kmooc.data_engineering.Email;
@@ -12,46 +13,23 @@ public class ArrayListExample15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(5);
-		list.add(3);
-		list.add(2);
-		list.add(4);
 		list.add(1);
-		System.out.println(list);
-	
-		list.sort(new MyIntegerComparator());
-		System.out.println(list);
-		
-		list.sort(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				if(o1 < o2) return +1;
-				else if(o1 == o2) return 0;
-				else return -1;
-			}
-		});
-		
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
 		System.out.println(list);
 		
-		ArrayList<Email> emails = new ArrayList<Email>();
-		emails.add(new Email(5,1));
-		emails.add(new Email(3,4));
-		emails.add(new Email(2,2));
-		emails.add(new Email(4,3));
-		emails.add(new Email(1,5));
+		List<Integer> subList = list.subList(2, 4);
+		System.out.println(subList);
 		
-		System.out.println(emails);
-		
-		emails.sort(new Comparator<Email>() {
-			@Override
-			public int compare(Email o1, Email o2) {
-				if(o1.getFrom() < o2.getFrom()) return -1;
-				else if(o1.getFrom() == o2.getFrom()) return 0;
-				else return 1;
-			}
-		});
-		
-		System.out.println(emails);
+		//subList에 추가한 내용은 list에 반영되지만,
+		//list에 추가한 후에 sublist를 출력하면 오류 발생
+		subList.add(1,9);
+		System.out.println(subList);
+		System.out.println(list);
 	}
 
 }
