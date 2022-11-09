7e88a875f515195d8d5d03681fdd8e32bd9aa1bd
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-11-09 16:45:33 +0900

    week10_2

--- src/main/java/data_engineering_hashmap/HashMapExample7.java
+++ src/main/java/data_engineering_hashmap/HashMapExample7.java
@@ -4,13 +4,19 @@ import java.util.HashMap;
 
 import kr.ac.sejong.kmooc.data_engineering.Email;
 
-public class HashMapExample5 {
+public class HashMapExample7 {
 	
 	public static void main(String[] args) {
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(1);
-		for(int i=0;i<1000;i++) {
-			map.put(i, i);
-		}
+		map.put(1, 3);
+		map.put(5, 7);
+		map.put(3, 9);
+		System.out.println(map);
+		map.remove(3);
+		System.out.println(map);
+		map.remove(2);
+		System.out.println(map);
+		map.clear();
 		System.out.println(map);
 	}
 
